46023ee8812c8dfb96fbf9aae7a621f431effa1f
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-11-02 12:38:47 +0300

    bootstrap

--- src/components/v-form-validate.vue
+++ src/components/v-form-validate.vue
@@ -1,65 +1,139 @@
 <template>
     <form class="v-form-validate row g-3">
         <div class="v-form-group">
-            <div class="col-md-6">
+            <div class="v-form-input  col-md-8">
             <label for="inputEmail4" class="form-label">Эл. адрес</label>
-            <input type="email" class="form-control" id="inputEmail4">
-        </div>
-        <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Пароль</label>
-            <input type="password" class="form-control" id="inputPassword4">
-        </div>
-        <div class="col-12">
-            <label for="inputAddress" class="form-label">Адрес</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="Проспект Ленина">
-        </div>
-        <div class="col-12">
-            <label for="inputAddress2" class="form-label">Адрес 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Квартира">
-        </div>
-        <div class="col-md-6">
-            <label for="inputCity" class="form-label">Город</label>
-            <input type="text" class="form-control" id="inputCity" placeholder="Санкт-Петербург">
-        </div>
-        <div class="col-md-4">
-            <label for="inputState" class="form-label">Область</label>
-            <select id="inputState" class="form-select">
-            <option selected>Выберите...</option>
-            <option>...</option>
-            </select>
-        </div>
-        <div class="col-md-2">
-            <label for="inputZip" class="form-label">Индекс</label>
-            <input type="text" class="form-control" id="inputZip">
-        </div>
-        <div class="col-12">
-            <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-                Проверить меня
-            </label>
+            <input 
+            type="email" 
+            class="form-control" 
+            id="inputEmail4" 
+            placeholder="email@mail.ru"
+            v-model.trim="email"
+            >
             </div>
-        </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">Войти в систему</button>
-        </div>
+            <div class="v-form-input col-md-8">
+                <label for="inputPassword4" class="form-label">Пароль</label>
+                <input 
+                type="password" 
+                class="form-control" 
+                id="inputPassword4" 
+                placeholder="************"
+                v-model.trim="password"
+                >
+            </div>
+            <div class="v-form-input col-md-8">
+                <label for="inputAddress" class="form-label">Адрес</label>
+                <input 
+                type="text" 
+                class="form-control" 
+                id="inputAddress" 
+                placeholder="Проспект Ленина"
+                v-model.trim="adres1"
+                >
+            </div>
+            <div class="v-form-input col-md-8">
+                <label for="inputAddress2" class="form-label">Адрес 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Квартира">
+            </div>           
+            <div class="v-form-input col-md-8">
+                <label for="inputState" class="form-label">Город</label>
+                <select id="inputState" class="form-select" v-model="city">
+                <option
+                 selected
+                 v-for="(city,index) in cityes"
+                 :key="index"
+                 >
+                 {{ city.label }}</option>                
+                </select>
+            </div>
+            <div class="v-form-input col-md-8">
+                <label for="inputZip" class="form-label">Индекс</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+            <div class="v-form-input col-md-8">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Проверить меня
+                </label>
+                </div>
+            </div>
+            <div class="v-form-input col-md-8">
+                <button type="submit" class="v-form-btn btn btn-primary">Войти в систему</button>
+            </div>
         </div>       
     </form>   
 </template>
 <script>
 
 export default {
-    name: 'vFormValidate'
+    name: 'vFormValidate',
+    data(){
+        return{
+            email: '',
+            password: '',
+            adres1: '',
+            city: [
+                {
+                    label: 'Москва',
+                    value: 'Moskva',
+                },
+                {
+                    label: 'Тула',
+                    value: 'Tula'
+                },
+                {
+                    label: 'Орёл',
+                    value: 'Orel'
+                }               
+            ]
+        }
+    },
+    methods:{
+        
+    }
 }
 
 </script>
 <style scoped>
     .v-form-validate{
-        max-width: 1620px;
+        max-width: 600px;
         margin: 0 auto;
-        border: 2px solid teal;
+        border: 4px solid #2f4f4f;
     }
     .v-form-group{
         padding: 50px;
+       display: flex;
+       flex-direction: column;
+       justify-content: center;
+       align-items: center;
+    }
+    .v-form-input{
+        margin-top: 20px;
+    }
+    .v-form-btn{
+        margin-left: 0;
+    }
+    .v-form-btn{
+        background-color: #778899;
+        color: white;
+        border: none;
+    }
+    .v-form-btn:hover{
+        transition: all ease-in .3s;
+        background-color: #2f4f4f;
+    }
+    label{
+        display: flex;
+        color: #2f4f4f;
+    }
+    input{
+        border: 3px solid teal;;
+    }
+    select{
+        border: 3px solid teal;;
+    }
+    option{
+        color: #2f4f4f;
     }
 </style>

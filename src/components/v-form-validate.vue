a5389544014f0afb8274b86dd299c7af1d8e02c9
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-11-03 12:03:26 +0300

    validate part1

--- src/components/v-form-validate.vue
+++ src/components/v-form-validate.vue
@@ -1,6 +1,7 @@
 <template>
     <form class="v-form-validate row g-3">
         <div class="v-form-group">
+            <!-- email -->
             <div class="v-form-input  col-md-8">
             <label for="inputEmail4" class="form-label">Эл. адрес</label>
             <input 
@@ -8,9 +9,10 @@
             class="form-control" 
             id="inputEmail4" 
             placeholder="email@mail.ru"
-            v-model.trim="email"
+            v-model.trim="form.email"
             >
             </div>
+            <!-- password -->
             <div class="v-form-input col-md-8">
                 <label for="inputPassword4" class="form-label">Пароль</label>
                 <input 
@@ -18,14 +20,15 @@
                 class="form-control" 
                 id="inputPassword4" 
                 placeholder="************"
-                v-model.trim="password"
+                v-model.trim="form.password"
                 >
             </div>
+            <!-- select1 -->
             <div class="v-form-input col-md-8">
                 <label for="inputState" class="form-label">Город</label>
                 <select id="inputState" 
                 class="form-select" 
-                v-model="city"
+                v-model="form.city"
                 >
                 <option
                  selected
@@ -36,12 +39,14 @@
                  {{ city.label }}</option>                
                 </select>
             </div>
+            <!-- select2 -->
             <div class="v-form-input col-md-8">
                 <label for="inputState" class="form-label">Адрес</label>
                 <select
                 class="form-select" 
                 id="inputState2" 
-                placeholder="Проспект Ленина"  
+                multiple
+                v-model="form.faforitAdres"
                 >
                 <option
                 v-for="(adres,index) in adreses"
@@ -55,26 +60,54 @@
                             
                 
             </div>
+            <!-- ФИО -->
             <div class="v-form-input col-md-8">
                 <label for="inputAddress2" class="form-label">ФИО</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="ФИО">
-            </div>           
+                <input 
+                type="text"
+                class="form-control"
+                id="inputAddress2"
+                placeholder="ФИО"
+                v-model.trim="$v.form.fio.$model"
+                   >
+            </div> 
+            <!-- phone -->
             <div class="v-form-input col-md-8">
                 <label for="inputAddress3" class="form-label">Телефон</label>
-                <input type="text" class="form-control" id="inputAddress3" placeholder="Телефон">
+                <input
+                type="text"
+                class="form-control" 
+                id="inputAddress3" 
+                placeholder="Телефон"
+                v-model.trim="form.phone"
+                >
             </div>  
+            <!-- Индекс -->
             <div class="v-form-input col-md-8">
                 <label for="inputZip" class="form-label">Индекс</label>
-                <input type="text" class="form-control" id="inputZip" placeholder="192158">
+                <input 
+                type="text" c
+                lass="form-control" 
+                id="inputZip" 
+                placeholder="192158"
+                v-model="form.indexPochta"
+                >
             </div>
+            <!-- checkbocks -->
             <div class="v-form-input col-md-8">
                 <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <input 
+                class="form-check-input" 
+                type="checkbox" 
+                id="gridCheck"
+                v-model="form.checkbocks"
+                >
                 <label class="form-check-label" for="gridCheck">
                     Проверить меня
                 </label>
                 </div>
             </div>
+            <!-- button -->
             <div class="v-form-input col-md-8">
                 <button type="submit" class="v-form-btn btn btn-primary">Войти в систему</button>
             </div>
@@ -83,13 +116,34 @@
 </template>
 <script>
 
+
+
 export default {
     name: 'vFormValidate',
+    validations () {
+    return {
+      form:{
+        fio:{
+            simpleValidator(value){
+                console.log(value)
+                return value.length >10
+            }
+        }
+      }
+      }
+    },
     data(){
         return{
-            email: '',
-            password: '',
-            city: 'Moskva',
+            form:{
+                fio: '',
+                phone: '',
+                email: '',
+                password: '',
+                indexPochta: '',
+                checkbocks: false,
+                city: 'Moskva',
+                faforitAdres: ['Mockovskii pr']
+            },          
             cityes: [
                 {
                     label: 'Москва',
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from '@vuelidate/core'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 Vue.use(BootstrapVue)
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
 new Vue({
